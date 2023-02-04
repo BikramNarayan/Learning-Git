e2000c4271d6c3f0a907d66c5fce220d2cb98cfc
Bikram <lcb2022029iiitl@.ac.in>
2023-02-04 23:42:14 +0530

    Added Multiply

---
+++ multiply.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int multiply(int a,int b){
+    return a*b;
+}
+int main(){
+    int a,b;
+    printf("Enter two number ");
+    cin>>a>>b;
+ cout<<"Multiplication of two numbers is  "<<multiply(a,b);
+ 
+ return 0;
+}

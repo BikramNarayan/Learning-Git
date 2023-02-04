26be0d862b64a5fcd8ab03ef27cebd94f13c9c6d
Bikram <lcb2022029iiitl@.ac.in>
2023-02-04 22:47:38 +0530

    Initial Commit

---
+++ sum.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int sum(int a,int b){
+    return a+b;
+}
+int main(){
+    int a,b;
+    printf("Enter two number ");
+    cin>>a>>b;
+ cout<<"Sum is "<<sum(a,b);
+ 
+ return 0;
+}

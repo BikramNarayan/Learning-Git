5f85e770c1c7e17afd32e108c1f83dc4aa7344b2
Bikram <lcb2022029iiitl@.ac.in>
2023-02-05 00:21:35 +0530

    Modifying multiply

--- multiply.cpp
+++ multiply.cpp
@@ -3,11 +3,17 @@ using namespace std;
 int multiply(int a,int b){
     return a*b;
 }
+int sum(int a,int b){
+    return a+b;
+}
 int main(){
     int a,b;
     printf("Enter two number ");
     cin>>a>>b;
  cout<<"Multiplication of two numbers is  "<<multiply(a,b);
+
+ cout<<"Addition of two numbers is  "<<sum(a,b);
+
  
  return 0;
 }
